ba9c4ea28c69725e8308e63dd15c0447007240d3
Dlama istifanus <dlamaistifanus0@gmail.com>
2020-06-07 07:31:04 +0100

    dlama

---
+++ dlama.js
@@ -0,0 +1,9 @@
+var fullName = 'Istifanus Dlama';
+
+var id = 'HNG-04224'
+
+var language = 'javascript';
+
+var email = 'dlamaistifanus0@gmail.com';
+
+alert('Hello, this is ' + fullName + ' with hngi7 id ' + id + ' using ' + language + ' for stage 2 task. ' + email);
